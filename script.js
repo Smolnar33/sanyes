3fdc3e2feaffaf7042320abf8c757c7f59cff43c
Smolnar33 <mol.sandor.92@gmail.com>
2023-12-06 14:42:42 +0200

    Implemented sticky header

--- script.js
+++ script.js
@@ -41,4 +41,28 @@ btnRight.addEventListener('click', nextSlide);
 
 btnLeft.addEventListener('click', prevSlide);
 
-setInterval(nextSlide, 5000);
+setInterval(nextSlide, 7000);
+
+////////// STICKY NAVIGATION /////////////
+
+const sectionSliderEl = document.querySelector('.slider');
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add('sticky');
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '20px',
+  }
+);
+
+obs.observe(sectionSliderEl);
